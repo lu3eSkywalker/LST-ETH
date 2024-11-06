c26b21dabd316fceda993dd238f1b3c096594294
lu3eSkywalker <surajjhajhria1@gmail.com>
2024-11-06 21:32:58 +0530

    Adding backend with mint and burn tokens logic

---
+++ server/src/index.ts
@@ -0,0 +1,88 @@
+import express from "express";
+import { ethers } from "ethers";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const RPC_URL = process.env.RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+const ABI = ["function mint(address, uint256)", "function burn(uint256)"];
+
+const providers = new ethers.JsonRpcProvider(RPC_URL);
+const wallet = new ethers.Wallet(PRIVATE_KEY || "", providers);
+const contract = new ethers.Contract(CONTRACT_ADDRESS || "", ABI, wallet);
+
+const VAULT = "0x728692C4936c2b6e24300dda3190B123A669EDb3";
+
+app.post("/alchemy", async (req, res) => {
+  const eventData = req.body.event;
+  if (!eventData || !eventData.activity) {
+    res.status(400).json({ error: "Invalid data format" });
+    return;
+  }
+
+  const incomingTxs = eventData.activity.find(
+    (x: any) => x.toAddress.toLowerCase() === VAULT.toLowerCase()
+  );
+
+  if (!incomingTxs) {
+    res.json({ message: "No transactions for the specified vault address" });
+    return;
+  }
+
+  const fromAddress = incomingTxs.fromAddress;
+  const valueToMint = ethers.parseUnits(incomingTxs.value.toString(), 18);
+  const valueToBurn = ethers.parseUnits(incomingTxs.value.toString(), 18);
+
+  if (incomingTxs.asset === "ETH") {
+    async function mintTokens() {
+      try {
+        const tx = await contract.mint(fromAddress, valueToMint);
+        await tx.wait();
+        console.log("Tokens minted:", tx.hash);
+        res.json({ message: "Mint successful", txHash: tx.hash });
+      } catch (error) {
+        console.error("Minting failed:", error);
+        res.status(500).json({ error: "Minting failed" });
+      }
+    }
+    await mintTokens();
+  } else {
+    async function burnTokens() {
+      try {
+        const tx = await contract.burn(valueToBurn);
+        await tx.wait();
+        console.log("Tokens burned:", tx.hash);
+
+        // Send Ether back to the sender
+        const tx2 = await wallet.sendTransaction({
+          to: fromAddress,
+          value: ethers.parseEther(incomingTxs.value.toString()),
+        });
+        await tx2.wait();
+
+        res.json({
+          message: "Token burn successful and Ether sent",
+          burnTxHash: tx.hash,
+          sendTxHash: tx2.hash,
+        });
+      } catch (error) {
+        console.error("Token Burn or Ether Send Failed", error);
+        res.status(500).json({ error: "Token Burn or Ether Send Failed" });
+      }
+    }
+    await burnTokens();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
